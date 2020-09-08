63bf33903e15a076eac7582ff75fd7b02840c64d
BossPokin <pokin67890@gmail.com>
2020-09-08 09:05:55 +0700

    add page folder

---
+++ src/page/UserPage.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class UserPage extends Component {
+
+  render() {
+
+    return (
+        <React.Fragment>
+          <h1>hello world</h1> 
+          <h1>hello world</h1>
+          <h1>hello world</h1>
+        </React.Fragment>
+    );
+  }
+
+}
+
+export default UserPage;
