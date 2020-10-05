3a630c6fdc8e5f28824484eb61d26cafd67f01ee
BossPokin <pokin67890@gmail.com>
2020-10-05 17:21:47 +0700

    Fix codes on Search Button

---
+++ src/component/ButtonWithIcon.jsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import "./Index.css"
+
+
+class ButtonWithIcon extends Component{
+
+    render() {
+        const {text, size, textSize, imageUrl} = this.props;
+
+        return (
+            <button class="buttonWithIcon">
+                <span>
+                    <img src={imageUrl} width="35" height = "35"></img>
+                    <span class="buttonWithIconText standard-font-style">{text}</span>
+                </span>
+            </button>
+        );
+    }
+}
+
+export default ButtonWithIcon;
--- src/page/UserPage.jsx
+++ src/page/UserPage.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../component/Header.jsx'
+import ButtonWithIcon from '../component/ButtonWithIcon.jsx'
+import search_logo from '../image/search.png'
 
 class UserPage extends Component {
   render() {
@@ -9,9 +11,15 @@ class UserPage extends Component {
           <Header text ={""} isDisplaySignUp={false} isDisplayRightButton={false}/>
           <div>
             <span class = "bar">
-              <span>
-                
+              {/* <span id = "findJob" class = "debug">
+                <img src = {search_logo} id = "searchLogo" width = "35" height = "35"></img>
               </span>
+              <span>
+                <p class = "Standard-font-style" id = "findJobText">find job</p>
+              </span> */}
+              <ButtonWithIcon text={"find job"} imageUrl={search_logo}>
+                HelloTest.
+              </ButtonWithIcon>
             </span>
           </div>
         </React.Fragment>
