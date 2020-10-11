5c52c12142316106e5176e5f3832ecb4c7e5c988
monNote <titivatmo@gmail.com>
2020-10-11 04:41:03 -0700

    change style for seacrh bar

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import CreateJobPage from './page/CreateJobPage/CreateJobPage.jsx'
+import CompanyPage from './page/CompanyPage/CompanyPage'
 
-ReactDOM.render( <CreateJobPage />, document.getElementById('root') );
+ReactDOM.render( <CompanyPage />, document.getElementById('root') );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
