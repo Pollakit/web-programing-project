f41d5e1b61dc40d795235727cf221915e43f4c96
Titivat <titivatmo@gmail.com>
2020-10-13 21:03:31 +0700

    add MainPage

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import * as serviceWorker from './serviceWorker';
 import FindJobPage from './page/FindJobPage.jsx'
 import CreateJobPage from './page/CreateJobPage/CreateJobPage.jsx'
 import MatchJobPage from './page/MatchJobPage.jsx';
-import UserPage from './page/UserPage';
+import UserPage from './page/UserPage.jsx';
+import MainPage from './page/MainPage.jsx'
 
-ReactDOM.render( <UserPage />, document.getElementById('root') );
+ReactDOM.render( <MainPage />, document.getElementById('root') );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
